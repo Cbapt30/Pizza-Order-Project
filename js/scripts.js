5b3a3ee7699a14e7ddedf7dd13e00fe0573056e8
Celia Baptiste <cfbaptiste@gmail.com>
2019-07-14 18:49:47 -0700

    add html, js, css

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+//Business Logic
+
+var toppings {
+  bacon: $1.50,
+  chicken: $1.50
+  blackOlives: $1.50
+  mushrooms: $1.50
+  artichokeHearts: $1.50
+  pineapple: $1.50
+};
+
+var pizzaTotalArray = [];
+
+pizzaTotalArray.forEach(function)
+
+
+
+
+//User Interface Logic
+$(document).ready(function()  {
+  $("form#pizza-creator").submit(function(event)) {
+    event.preventDefault();
+
+    parseInt($("input:checkbox[value=toppings]:checked").val();
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+}
